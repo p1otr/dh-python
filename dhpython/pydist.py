17483ffa48617760f402a431daec4ecb7e87c09b
Piotr Ożarowski <piotr@debian.org>
2014-10-26 22:13:31 +0100

    support "dist (version req)" syntax in REQUIRES_RE
    this is used in egg-info file's Requires line(s)

--- dhpython/pydist.py
+++ dhpython/pydist.py
@@ -49,11 +49,13 @@ REQUIRES_RE = re.compile(r'''
     \s*
     (?P<enabled_extras>(?:\[[^\]]*\])?)  # ignored for now
     \s*
+    \(?  # optional parenthesis
     (?:  # optional minimum/maximum version
         (?P<operator><=?|>=?|==|!=)
         \s*
         (?P<version>(\w|[-.])+)
     )?
+    \)?  # optional closing parenthesis
     ''', re.VERBOSE)
 
 
@@ -121,7 +123,7 @@ def guess_dependency(impl, req, version=None):
         version = Version(version)
 
     # some upstreams have weird ideas for distribution name...
-    name, rest = re.compile('([^><= \[]+)(.*)').match(req).groups()
+    name, rest = re.compile('([^><= \(\)\[]+)(.*)').match(req).groups()
     req = safe_name(name) + rest
 
     data = load(impl)
