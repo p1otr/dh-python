ef2d41713893ddee15036cc0b0d295980d828017
Piotr Ożarowski <piotr@debian.org>
2018-07-11 23:30:47 +0200

    dh_python{2,3}: fix \.so.* symlink renaming for non-default Python versions

--- dhpython/fs.py
+++ dhpython/fs.py
@@ -71,20 +71,9 @@ def share_files(srcdir, dstdir, interpreter, options):
             # try to rename extension here as well (in :meth:`scan` info about
             # Python version is gone)
             version = interpreter.parse_public_dir(srcdir)
-            # if version is True it means it's unversioned dist-packages dir
             if version and version is not True:
-                # note that if ver is empty, default Python version will be used
-                fpath1_orig = fpath1
-                new_name = interpreter.check_extname(i, version)
-                if new_name:
-                    fpath1 = join(srcdir, new_name)
-                    if exists(fpath1):
-                        log.warn('destination file exist, '
-                                 'cannot rename %s to %s', fpath1_orig, fpath1)
-                    else:
-                        log.info('renaming %s to %s', fpath1_orig, fpath1)
-                        os.renames(fpath1_orig, fpath1)
-                        i = new_name
+                fpath1 = Scan.rename_ext(fpath1, interpreter, version)
+                i = split(fpath1)[-1]
         fpath2 = join(dstdir, i)
         if not isdir(fpath1) and not exists(fpath2):
             # do not rename directories here - all .so files have to be renamed first
@@ -209,7 +198,7 @@ class Scan:
                 fext = splitext(fn)[-1][1:]
                 if fext == 'so':
                     if not self.options.no_ext_rename:
-                        fpath = self.rename_ext(fpath)
+                        fpath = self.rename_ext(fpath, interpreter, version)
                     ver = self.handle_ext(fpath)
                     ver = ver or version
                     if ver:
@@ -286,13 +275,17 @@ class Scan:
             if dpath.startswith(join('debian', self.package, i)):
                 return '/' + i
 
-    def rename_ext(self, fpath):
+    @staticmethod
+    def rename_ext(fpath, interpreter, current_pub_version=None):
         """Add multiarch triplet, etc. Return new name.
 
         This method is invoked for all .so files in public or private directories.
         """
+        # current_pub_version - version parsed from dist-packages (True if unversioned)
+        # i.e. if it's not None - it's a public dist-packages directory
+
         path, fname = fpath.rsplit('/', 1)
-        if self.current_dir_is_public and islink(fpath):
+        if current_pub_version is not None and islink(fpath):
             # replace symlinks with extensions in dist-packages directory
             dstfpath = fpath
             links = set()
@@ -311,7 +304,7 @@ class Scan:
             # ignore /lib/i386-linux-gnu/, /usr/lib/x86_64-kfreebsd-gnu/, etc.
             return fpath
 
-        new_fn = self.interpreter.check_extname(fname, self.current_pub_version)
+        new_fn = interpreter.check_extname(fname, current_pub_version)
         if new_fn:
             # TODO: what about symlinks pointing to this file
             new_fpath = join(path, new_fn)
